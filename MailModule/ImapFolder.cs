b02c4f0dd4d615e1a86a45abd0287575847f38a5
Contact <contact@rendezz.com>
2017-11-27 16:17:25 +0200

    Added some missing folders for empty mailboxes

---
+++ MailModule/ImapFolder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using S22.Imap;
+
+namespace Zinkuba.MailModule
+{
+    internal class ImapFolder
+    {
+        internal String MappedDestination;
+        internal MailboxInfo FolderPath;
+        internal bool IsPublicFolder;
+    }
+}
